ec88bca4959bb2e0ff0537e3527b02b96afb6796
Febry Ardiansyah <febrymuhammad80@gmail.com>
2022-07-15 20:54:36 +0700

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const db = require('./src/database/database')
+const userRoutes = require('./src/routes/user.route')
+const storyRoutes = require('./src/routes/story.route')
+
+const PORT = process.env.PORT || 3000
+
+db.sync().then(()=>{
+    console.log(`Database synced`);
+}).catch((err)=>{
+    console.log(`Database failed sync: ${err}`);
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(userRoutes)
+app.use(storyRoutes)
+
+app.use('*',(req,res)=>{
+    return res.status(404).send({
+        message: 'API path not found'
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server listening on ${PORT}`);
+})
